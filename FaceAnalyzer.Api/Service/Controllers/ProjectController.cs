f4a495e707cf67bc7fcb810549f1c2a93b1da712
Awad Osman <awadosman997@gmail.com>
2023-10-24 21:16:20 +0200

    [projects]: project creation

---
+++ FaceAnalyzer.Api/Service/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using FaceAnalyzer.Api.Business.BusinessModels;
+using FaceAnalyzer.Api.Business.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceAnalyzer.Api.Service.Controllers;
+
+[Route("projects")]
+public class ProjectController : ControllerBase
+{
+    private readonly ProjectBusinessModel _businessModel;
+
+    public ProjectController(ProjectBusinessModel businessModel)
+    {
+        _businessModel = businessModel;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProjectDto>> Create([FromBody] ProjectDto dto)
+    {
+        var project = await _businessModel.Create(dto);
+        // var result = new ActionResult<ProjectDto>(project);
+
+        return Created($"/projects/{project.Id}", project);
+    }
+}
