c85158ef745bcff729e78a60ad1c15459c3d8244
Awad Osman <awadosman997@gmail.com>
2023-11-01 12:41:19 +0100

    auto generated UpdatedAt and CreatedAt columns

---
+++ FaceAnalyzer.Api/Data/EFCoreExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaceAnalyzer.Api.Data;
+
+public static class EFCoreExtensions
+{
+    public static void AddDeletedAtQueryFilter(this ModelBuilder modelBuilder)
+    {
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var deletedAtPropertyName = nameof(IDeletable.DeletedAt);
+            var deletedAtProperty = entityType.FindProperty(deletedAtPropertyName);
+
+            if (deletedAtProperty != null)
+            {
+                // (IDeletable entity) => entity.DeletedAt == null
+                var parameter = Expression.Parameter(entityType.ClrType);
+                var deletedAtExpression = Expression.Lambda(
+                    Expression.Equal(
+                        Expression.Property(parameter, deletedAtProperty.PropertyInfo),
+                        Expression.Constant(null, deletedAtProperty.ClrType)
+                    ),
+                    parameter
+                );
+
+                modelBuilder.Entity(entityType.ClrType)
+                    .HasQueryFilter(deletedAtExpression);
+            }
+        }
+    }
+
+    public static void AddAutoCreatedAt(this ModelBuilder modelBuilder)
+    {
+        const string createdAtPropertyName = nameof(EntityBase.CreatedAt);
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var createdAt = entityType
+                        .FindProperty(createdAtPropertyName);
+            if (createdAt != null)
+            {
+                modelBuilder.Entity(entityType.ClrType)
+                    .Property(createdAtPropertyName)
+                    .HasColumnType("TIMESTAMP")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            }
+        }
+    }
+
+    public static void AddAutoUpdatedAt(this ModelBuilder modelBuilder)
+    {
+        const string updatedAtPropertyName = nameof(EntityBase.UpdatedAt);
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var updatedAtProperty = entityType.FindProperty(updatedAtPropertyName);
+            if (updatedAtProperty != null)
+            {
+                modelBuilder.Entity(entityType.ClrType)
+                    .Property(updatedAtPropertyName)
+                    .HasColumnType("TIMESTAMP")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+            }
+        }
+    }
+}
