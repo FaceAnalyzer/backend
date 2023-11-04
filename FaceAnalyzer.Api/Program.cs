6269d4cd9c557c26d6fe8db459b63b24aa094dab
plavy <tinplavec@gmail.com>
2023-11-04 19:38:42 +0100

    add dockerfile, manifest, makefile, deployment works

--- FaceAnalyzer.Api/Program.cs
+++ FaceAnalyzer.Api/Program.cs
@@ -26,11 +26,8 @@ var app = builder.Build();
 
 #region Pipeline
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
