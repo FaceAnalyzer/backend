53418f4f80e3e0c318993354c0b820faafb29c9e
Abdelrahman Dafalla <daffallahabdulrahman@gmail.com>
2023-12-10 13:40:38 +0100

    - EmotionController.cs documentation.

--- FaceAnalyzer.Api/Service/Controllers/EmotionController.cs
+++ FaceAnalyzer.Api/Service/Controllers/EmotionController.cs
@@ -2,11 +2,13 @@ using FaceAnalyzer.Api.Business.Commands.Emotions;
 using FaceAnalyzer.Api.Business.Contracts;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace FaceAnalyzer.Api.Service.Controllers;
 
 [ApiController]
 [Route("emotions")]
+[SwaggerResponse(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
 public class EmotionController: ControllerBase
 {
 
@@ -18,6 +20,9 @@ public class EmotionController: ControllerBase
     }
 
     [HttpPost]
+    [SwaggerOperation("Create an Emotion entry.",
+        "Create an emotion entry, given an emotion type, and a value. The created emotion is associated with a Reaction.",
+        OperationId = $"{nameof(EmotionController)}_create")]
     public async Task<ActionResult<EmotionDto>> CreateEmotion([FromBody] CreateEmotionCommand request)
     {
         var emotionDto = await _mediator.Send(request);
